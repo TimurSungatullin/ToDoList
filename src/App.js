7c56937a2098b1fda268b0e9e939d97fa01c52aa
Тимур Сунгатуллин <arkana1223@mail.ru>
2019-09-26 18:27:35 +0300

    React project start

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div class="center">
+        0==============8
+    </div>
+  );
+}
+
+export default App;
